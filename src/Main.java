afea9abff297442abb88d6389ef30ff1a9e5fd22
Davee Nguyen <davee@daveenguyen.com>
2015-09-14 01:22:04 -0700

    Added comments and print HELLO!

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// This prints Hello World!
 		System.out.println("Hello World!");
+		// This prints HELLO!
+		System.out.println("HELLO!");
 	}
 
 }
